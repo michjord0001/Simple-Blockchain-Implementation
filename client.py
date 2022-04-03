14a9a720d55a12c208f54b83120f2308e2141ea8
MJ <michjord0001@pm.me>
2022-04-03 13:58:52 +0200

    20220403 Message protocol implementation

--- client.py
+++ client.py
@@ -1,16 +1,34 @@
 import socket
 import pandas
 
+FORMAT = "utf-8"
+
 def loadSeedNodes():
     df = pandas.read_csv('seedNodes.csv', usecols=['IP', 'Port']);
     return(df)
 
+#def discoverOtherNodes():
+
+def broadcastMessage(message):
+    stx = ascii(2).encode(FORMAT) #Encode Start Transaction
+    cmd = message.encode(FORMAT) #Encode command.
+    cmd_len = str(len(cmd)).encode(FORMAT) #Encode length of encoded command. 
+    etx = ascii(3).encode(FORMAT) #Encode End Transaction
+    s.send(stx)
+    s.send(cmd_len)
+    s.send(cmd)
+    s.send(etx)
+
+def processIncomingMessages():
+    data, addr = s.recvfrom(1024)
+    print("Message [" + data.decode("utf-8") + "] from [" + str(seedNodes.IP[0]) + "]:[" + str(seedNodes.Port[0]) + "]")
+
+#Load servers and ports
 seedNodes = loadSeedNodes()
 
 #Connect to socket. 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((seedNodes.IP[0], seedNodes.Port[0]))
 
-#Receive messages. 
-data, addr = s.recvfrom(1024)
-print("Message [" + data.decode("utf-8") + "] from [" + str(seedNodes.IP[0]) + "]:[" + str(seedNodes.Port[0]) + "]")
+processIncomingMessages()
+broadcastMessage("UprootTheSystem")
--- server.py
+++ server.py
@@ -1,19 +1,77 @@
-import socket 
+import socket   
 import pandas
+import time 
+import sys
+import threading
+#import getopt
+
+HEADER = 1
+FORMAT = 'utf-8'
 
 def loadSeedNodes():
-    df = pandas.read_csv('seedNodes.csv', usecols=['IP', 'Port']);
+    df = pandas.read_csv('seedNodes.csv', usecols=['IP', 'Port'])
     return(df)
 
+def scheduleNextMessage(clientsocket, address):
+    clientsocket.send(bytes("System", FORMAT))
+    time.sleep(5)
+
+def handleUserActions(clientsocket, address):
+    print(f"Connected with {address[0]}:{address[1]}")
+    
+    connected = True
+    while connected:
+        try:
+            stx = clientsocket.recv(HEADER).decode(FORMAT)
+            cmd_len = clientsocket.recv(HEADER).decode(FORMAT)
+            cmd = clientsocket.recv(int(cmd_len)).decode(FORMAT)
+            etx = clientsocket.recv(HEADER).decode(FORMAT) 
+            #msg = clientsocket.recv(HEADER).decode(FORMAT)
+            #msgList = msg.split()              
+            #stx = str(msgList[0])
+            #cmd_len = str(msgList[1])
+            #cmd = str(msgList[2])
+            #etx = str(msgList[3])       
+        except:
+            print("Error interpretting message protocol.")
+            sys.exit()   
+            
+        print(f'Message: {stx} {cmd_len} {cmd} {etx}')
+
+        if stx != "2": print(f"Invalid input for <STX>"); sys.exit()  
+                
+        #Insert switch statement for cmd
+
+        if (etx == '3'): connected = False
+    clientsocket.close()
+
+#Protocol design (i): Require username argument. 
+def verifyInput():
+    if(len(sys.argv) < 2):
+        print("Require username argument.") 
+        sys.exit()
+    if(len(sys.argv[1]) > 2):
+        print("Invalid username length.") 
+        sys.exit()
+
+#Load servers and ports
 seedNodes = loadSeedNodes()
 
 #Create socket, bind and listen. 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((seedNodes.IP[0], seedNodes.Port[0]))
 s.listen(10)
+print(f'Listening on {seedNodes.IP[0]}:{seedNodes.Port[0]}')
 
-#Send messages.
+#Receive messages.
 while True:
+    verifyInput()
     clientsocket, address = s.accept()
-    print(f"Established connection with {address}.")
-    clientsocket.send(bytes("#UprootTheSystem", "utf-8"))
+    #Broadcast message
+    scheduleNextMessage(clientsocket, address)
+
+    #Thread connections from clients
+    thread = threading.Thread(target=handleUserActions, args=(clientsocket, address))
+    thread.start()
+
+
